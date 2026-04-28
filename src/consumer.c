1e80c420ec469e58b2247cdf3d2dfbecf44f572f
g00100761-spec <g00100761@aus.edu>
2026-04-28 09:19:02 +0300

    Fix consumer_thread function implementation

--- src/consumer.c
+++ src/consumer.c
@@ -4,19 +4,17 @@ static void *consumer_thread(void *arg) {
     buffer_t *buf = cargs->buf;
 
     while (1) {
-      
         item_t item = buffer_remove(buf);
 
-        
         if (item.value == POISON_PILL) {
             printf("[Consumer-%d] got poison pill, shutting down\n", id);
             break;
         }
 
-        
         struct timespec dequeue_ts;
         clock_gettime(CLOCK_MONOTONIC, &dequeue_ts);
-        double latency = timespec_diff_ms(item.enqueue_ts,
+        double latency = timespec_diff_ms(item.enqueue_ts, dequeue_ts);
+
         if (item.priority == URGENT) {
             printf("[Consumer-%d] Consumed item: %d [URGENT] (latency: %.2f ms)\n",
                    id, item.value, latency);
@@ -25,7 +23,6 @@ static void *consumer_thread(void *arg) {
                    id, item.value, latency);
         }
 
-  
         pthread_mutex_lock(&g_metrics.mtx);
         g_metrics.total_consumed++;
         g_metrics.total_latency_ms += latency;
